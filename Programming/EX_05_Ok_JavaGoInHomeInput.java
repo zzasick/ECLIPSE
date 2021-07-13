989b696038c5424528fac81ca11b37a55cdea983
zzasick <86180957+zzasick@users.noreply.github.com>
2021-07-13 18:41:15 +0900

    JAVA
    JAVA

---
+++ Programming/EX_05_Ok_JavaGoInHomeInput.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.DimmingLights;
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class EX_05_Ok_JavaGoInHomeInput {
+
+	public static void main(String[] args) { // Parameter (type of variable) - between program and program user
+		
+		String id = args[0];
+		String bright = args[1];
+
+		// Elevator call
+		Elevator myElevator = new Elevator (id);
+		myElevator.callForUp(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// Light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / floor Lamp");
+		floorLamp.on();
+		
+		DimmingLights moodLamp = new DimmingLights(id+" moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		moodLamp.on();
+		
+	}
+
+}
